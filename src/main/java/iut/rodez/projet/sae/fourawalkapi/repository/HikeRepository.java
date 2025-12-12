ae0acf2c462b53b0815c33aa4f2d59aae494cd4a
Alexandreiut <alexandre.brouzes@iut-rodez.fr>
2025-12-12 14:17:35 +0100

    Création du service pour les randonnés mise à jour du repository

--- src/main/java/iut/rodez/projet/sae/fourawalkapi/repository/HikeRepository.java
+++ src/main/java/iut/rodez/projet/sae/fourawalkapi/repository/HikeRepository.java
@@ -4,5 +4,15 @@ import iut.rodez.projet.sae.fourawalkapi.entity.Hike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface HikeRepository extends JpaRepository<Hike, Long> {}
+public interface HikeRepository extends JpaRepository<Hike, Long> {
+
+    /**
+     * Recherche toutes les randonnées créées par un utilisateur donné (UC1 : Liste des randonnées créées).
+     * @param creatorId L'ID de l'utilisateur qui a créé la randonnée.
+     * @return La liste des randonnées.
+     */
+    List<Hike> findByCreatorId(Long creatorId);
+}
