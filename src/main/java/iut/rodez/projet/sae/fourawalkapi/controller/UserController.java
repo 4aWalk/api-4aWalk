d9d419a590b14dbcb24cf09facd9fbad727f09e6
Alexandreiut <alexandre.brouzes@iut-rodez.fr>
2026-01-24 21:58:23 +0100

    update de l'appellation end point user
    conformité et mise en cohérence avec tous les end points

--- src/main/java/iut/rodez/projet/sae/fourawalkapi/controller/UserController.java
+++ src/main/java/iut/rodez/projet/sae/fourawalkapi/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 // Indique que c'est une API REST
 @RestController
 // Chemin de base pour les requêtes
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
