1ed203513a00fd05014e7b94acd853791c02b7b8
Alexandreiut <alexandre.brouzes@iut-rodez.fr>
2026-03-17 23:53:31 +0100

    Update HikeServiceTest.java

--- src/test/java/iut/rodez/projet/sae/fourawalkapi/service/HikeServiceTest.java
+++ src/test/java/iut/rodez/projet/sae/fourawalkapi/service/HikeServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.*;
 
@@ -76,6 +77,7 @@ class HikeServiceTest {
         testHike.setCreator(testUser);
         testHike.setDepart(poiDepart);
         testHike.setArrivee(poiArrivee);
+        ReflectionTestUtils.setField(hikeService, "entityManager", entityManager);
     }
 
     // ==========================================
